b81335511024620daa89462165a3923e840014a3
Markus Karlsson <markuskarlsson__@hotmail.com>
2019-11-17 22:32:29 +0100

    First steps

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Lab-2
 Ett interaktivt digitalt CV.
+
+## Beskrivning av uppgiften
+I den här laborationen skall du skapa ett interaktivt digitalt CV där du presenterar dig själv och dina kunskaper. Till exempel kan du beskriva vem du är med bild och text, skapa en portfolio med dina projekt, kanske till och med inkludera en länk till ditt GitHub konto? Uppgiften är fri och du väljer själv hur din sida ska se ut och hur användaren interagerar med sidan. Låt kreativiteten flöda!
+
+## GitHub
+Länk till repot: https://github.com/markusakarlsson/Lab-2
+
+Länk till pages: https://markusakarlsson.github.io/Lab-2/
---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', loadPage)
+
+/**
+ * 
+ */
+function loadPage() {
+    textTypingEffectHeroText()
+}
+
+/**
+ * Starts a loop that displays the header with a typewriter animation
+ */
+function textTypingEffectHeroText() {
+    let i = 0
+    const txt = "<h1>Hi, I'm Markus Karlsson</h1>"
+    const speed = 125
+    const element = document.getElementById("welcome-text")
+    const interval = setInterval(addCharacter, speed)
+
+    function addCharacter() {
+        if (i < txt.length) {
+            element.innerHTML += txt.charAt(i)
+            i++
+        } else {
+            clearInterval(interval)
+        }
+    }
+}
